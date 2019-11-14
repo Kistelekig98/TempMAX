5a2817c5935407d0dfd4080f4e40802b91c94f6a
Kistelekig98 <Kistelekig98@users.noreply.github.com>
2019-11-14 10:11:23 +0100

    Add basic express frontend

---
+++ TempMAX-frontend/index.js
@@ -0,0 +1,9 @@
+﻿var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(express.static('static'));
+
+var server = app.listen(3000, function () {
+    console.log("On :3000");
+});
---
+++ TempMAX-frontend/static/js/temps.js
@@ -0,0 +1,27 @@
+﻿const uri = "http://localhost:8080/temps";
+//const uri = "https://jsonplaceholder.typicode.com/users";
+
+$(document).ready(function () {
+    listAllTemps();
+});
+
+function listAllTemps() {
+	$.ajax({
+		type: 'GET',
+		url: uri,
+		cache: false,
+		success: function (list) {
+			$.each(list, function (i, item) {
+				$('#tempList').append(
+					"<li class='list-group-item'> <div class='row'>" +
+						"<div class='col'>" + item.place + "</div>" +	// item.place
+						"<div class='col'>" + item.temperature + "</div>" +		// item.temperature
+					"</div> </li>"
+				);
+			});
+		},
+		error: function (jqXHR, status, error) {
+			alert("jqXHR: " + jqXHR.status + "\n\nstatus: " + status + "\n\nerror: " + error);
+		}
+	});
+}
