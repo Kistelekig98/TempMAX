198912584d406d03b391999d250e9fa3bbabbfac
Nyizsnyik Benjámin <nyizsnyik.benjamin@gmail.com>
2019-12-04 11:44:20 +0100

    add test forecasts

--- TempMAX-backend/src/main/java/com/agile/tempmax/TempMAX/backend/helpers/InitDB.java
+++ TempMAX-backend/src/main/java/com/agile/tempmax/TempMAX/backend/helpers/InitDB.java
@@ -1,6 +1,8 @@
 package com.agile.tempmax.TempMAX.backend.helpers;
 
+import com.agile.tempmax.TempMAX.backend.model.Forecast;
 import com.agile.tempmax.TempMAX.backend.model.WeatherDetail;
+import com.agile.tempmax.TempMAX.backend.repository.ForecastRepository;
 import com.agile.tempmax.TempMAX.backend.repository.WeatherRepository;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,15 +10,23 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
 @Component
 public class InitDB implements ApplicationRunner {
     @Setter(onMethod=@__({@Autowired}))
     private WeatherRepository weatherRepository;
 
+    @Setter(onMethod=@__({@Autowired}))
+    private ForecastRepository forecastRepository;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        weatherRepository.save(new WeatherDetail("Budapest", 18.5,0.4,30,10,44.333,33.444));
-        weatherRepository.save(new WeatherDetail("Szentendre", -10,0.1,10,13.4,34.33,55.56));
+        weatherRepository.save(new WeatherDetail("Budapest", 0,0.4,30,10,44.333,33.444));
+        weatherRepository.save(new WeatherDetail("Szentendre", -5,0.1,10,13.4,34.33,55.56));
+
+        forecastRepository.save(new Forecast("Budapest előrejelzés", "Elég hideg lesz...", new Date()));
+        forecastRepository.save(new Forecast("Szentendre előrejelzés", "Itt is elég hideg lesz...", new Date()));
     }
 
 }
